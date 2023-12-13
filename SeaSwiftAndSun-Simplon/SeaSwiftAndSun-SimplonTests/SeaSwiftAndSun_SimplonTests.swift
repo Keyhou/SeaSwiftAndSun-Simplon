68d5b302e66210b26959dd3ebf8b4990070751ed
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2023-12-13 12:08:04 +0100

    clean test

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-SimplonTests/SeaSwiftAndSun_SimplonTests.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-SimplonTests/SeaSwiftAndSun_SimplonTests.swift
@@ -8,14 +8,6 @@
 import XCTest
 
 final class SeaSwiftAndSun_SimplonTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     func testLoadedImage() async throws {
         var spotImage = await UIImageView()
         let imageURL = URL(string: "https://source.unsplash.com/random/300x200")!
@@ -23,12 +15,4 @@ final class SeaSwiftAndSun_SimplonTests: XCTestCase {
 
         XCTAssertNotNil(imageFetcher)
     }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
