f9a15da385327fed2ac725a12c8619c4ab6dbb50
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2023-12-13 12:16:17 +0100

    correction

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-SimplonTests/SeaSwiftAndSun_SimplonTests.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-SimplonTests/SeaSwiftAndSun_SimplonTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class SeaSwiftAndSun_SimplonTests: XCTestCase {
     func testLoadedImage() async throws {
-        var spotImage = await UIImageView()
+        let spotImage = await UIImageView()
         let imageURL = URL(string: "https://source.unsplash.com/random/300x200")!
         let imageFetcher: () =  await spotImage.load(url: (imageURL))
 
