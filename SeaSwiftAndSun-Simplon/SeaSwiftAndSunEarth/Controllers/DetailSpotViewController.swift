69d603f7d1544c13f118018edbb07950cbfc6ba0
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-12-14 18:01:07 +0100

    Work on constraints and alignments in Detail View

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSunEarth/Controllers/DetailSpotViewController.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSunEarth/Controllers/DetailSpotViewController.swift
@@ -1,13 +1,17 @@
 import UIKit
 
 class DetailSpotViewController: UIViewController {
+    
+    // MARK: - Properties
     var selectedSpot: Spot?
     var selectedImage: UIImage?
     
+    // MARK: - IBOutlets
     @IBOutlet weak var detailsView: UIView!
     @IBOutlet weak var spotName: UILabel!
     @IBOutlet weak var spotImage: UIImageView!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -15,11 +19,10 @@ class DetailSpotViewController: UIViewController {
         if let spot = selectedSpot {
             setUpUI(spot: spot)
         }
-        
         self.detailsView.accessibilityIdentifier = "DetailSpotView"
     }
     
-    // Set up the basic UI elements based on the selected spot
+    // MARK: - UI Setup
     func setUpUI(spot: Spot) {
         spotImage.image = selectedImage
         spotImage.layer.cornerRadius = spotImage.frame.size.width / 2
@@ -28,55 +31,12 @@ class DetailSpotViewController: UIViewController {
         // Set up the detailed information view for the spot
         setUpSpotDetails(spot: spot)
     }
-    private func createLabelWithBackground(text: String, backgroundColor: UIColor, cornerRadius: CGFloat) -> UILabel {
-        
-        let label = UILabel()
-        label.text = text
-        label.backgroundColor = backgroundColor
-        label.layer.cornerRadius = cornerRadius
-        label.clipsToBounds = true
-        label.translatesAutoresizingMaskIntoConstraints = false
     
-        
-        return label
-    }
-
-    // Helper function to create an image view with a system icon
-    private func createIconImageView(systemName: String) -> UIImageView {
-        let imageView = UIImageView(image: UIImage(systemName: systemName))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }
-    
-    // Helper function to create a label with specified text and alignment
-    private func createLabel(text: String, alignment: NSTextAlignment = .left) -> UILabel {
-        let label = UILabel()
-        label.text = text
-        label.textAlignment = alignment
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }
-    
-    // Helper function to determine background color based on surf break
-    private func surfBreakBackgroundColor(surfBreak: String?) -> UIColor {
-        switch surfBreak {
-        case "Reef Break":
-            return .cyan
-        case "Point Break":
-            return .systemMint
-        case "Beach Break":
-            return .systemTeal
-        case "Outer Banks":
-            return .systemPink
-        default:
-            return .systemTeal
-        }
-    }
-    // Set up the detailed information view for the spot, including peak surf season and contact details
+    // MARK: - Surf Break Details
     private func setUpSpotDetails(spot: Spot) {
         // Peak surf season details
         let peakSurfSeason: [(title: String, dates: String)] = [
-            ("Peak Surf Season", "\(spot.fields.peakSurfSeasonBegins)・\(spot.fields.peakSurfSeasonEnds)")
+            ("Peak Season", "\(spot.fields.peakSurfSeasonBegins)・\(spot.fields.peakSurfSeasonEnds)")
         ]
         
         var surfBreakView: UIView?
@@ -98,23 +58,33 @@ class DetailSpotViewController: UIViewController {
             detailsView.addSubview(surfBreakContainerView!)
 
            
-            surfBreakContainerView!.topAnchor.constraint(equalTo: detailsView.topAnchor).isActive = true
-                surfBreakContainerView!.leftAnchor.constraint(equalTo: detailsView.leftAnchor, constant: 4).isActive = true
-                surfBreakContainerView!.bottomAnchor.constraint(lessThanOrEqualTo: detailsView.bottomAnchor).isActive = true
-                surfBreakContainerView!.rightAnchor.constraint(lessThanOrEqualTo: detailsView.rightAnchor).isActive = true
-         
+            surfBreakContainerView!.topAnchor.constraint(equalTo: detailsView.topAnchor, constant: 10).isActive = true
+            surfBreakContainerView!.leadingAnchor.constraint(equalTo: detailsView.leadingAnchor, constant: 10).isActive = true
+            surfBreakContainerView!.bottomAnchor.constraint(lessThanOrEqualTo: detailsView.bottomAnchor).isActive = true
+            surfBreakContainerView!.trailingAnchor.constraint(lessThanOrEqualTo: detailsView.trailingAnchor, constant: -10).isActive = true
 
-                surfBreakView!.centerYAnchor.constraint(equalTo: surfBreakContainerView!.centerYAnchor).isActive = true
-                surfBreakView!.topAnchor.constraint(equalTo: surfBreakContainerView!.topAnchor, constant: 10).isActive = true
-            surfBreakView!.leftAnchor.constraint(equalTo: surfBreakContainerView!.leftAnchor, constant: 10).isActive = true
-                surfBreakView!.heightAnchor.constraint(equalToConstant: 20).isActive = true            
-            surfBreakView!.bottomAnchor.constraint(equalTo: surfBreakContainerView!.bottomAnchor).isActive = true
-                surfBreakView!.rightAnchor.constraint(equalTo: surfBreakContainerView!.rightAnchor, constant: 10).isActive = true
+            surfBreakView!.topAnchor.constraint(equalTo: surfBreakContainerView!.topAnchor, constant: 10).isActive = true
+            surfBreakView!.leadingAnchor.constraint(equalTo: surfBreakContainerView!.leadingAnchor, constant: 10).isActive = true
+            surfBreakView!.heightAnchor.constraint(equalToConstant: 20).isActive = true
+            surfBreakView!.bottomAnchor.constraint(equalTo: surfBreakContainerView!.bottomAnchor, constant: -10).isActive = true
+            surfBreakView!.trailingAnchor.constraint(equalTo: surfBreakContainerView!.trailingAnchor, constant: -10).isActive = true
+
            
         }
-        
+        // Difficulty level stars
+        let difficultyLevel = spot.fields.difficultyLevel
+        let starsRow = createStarsRow(difficultyLevel: difficultyLevel)
+        detailsView.addSubview(starsRow)
+
         var previousView: UIView = detailsView
         previousView = surfBreakContainerView ?? detailsView
+
+        // Add constraints for stars row
+        NSLayoutConstraint.activate([
+            starsRow.topAnchor.constraint(equalTo: previousView.topAnchor, constant: 10),
+            starsRow.trailingAnchor.constraint(equalTo: detailsView.trailingAnchor, constant: -10),
+            starsRow.heightAnchor.constraint(equalToConstant: 25)
+        ])
         
         for (title, dates) in peakSurfSeason {
             // Create clock icon image view
@@ -130,22 +100,22 @@ class DetailSpotViewController: UIViewController {
             detailsView.addSubview(scheduleLabel)
             
             // Add constraints for clock icon, title, and schedule labels
-            NSLayoutConstraint.activate([
-                clockImageView.topAnchor.constraint(equalTo: surfBreakContainerView!.bottomAnchor, constant: 4),
-                clockImageView.leftAnchor.constraint(equalTo: detailsView.leftAnchor, constant: 4),
-                clockImageView.widthAnchor.constraint(equalToConstant: 12),
-                clockImageView.heightAnchor.constraint(equalToConstant: 12),
-                
-                titleLabel.centerYAnchor.constraint(equalTo: clockImageView.centerYAnchor),
-                titleLabel.leftAnchor.constraint(equalTo: clockImageView.rightAnchor, constant: 8),
-                titleLabel.widthAnchor.constraint(equalToConstant: 220),
-                titleLabel.heightAnchor.constraint(equalToConstant: 25),
-                
-                scheduleLabel.centerYAnchor.constraint(equalTo: clockImageView.centerYAnchor),
-                scheduleLabel.rightAnchor.constraint(equalTo: detailsView.rightAnchor),
-                scheduleLabel.widthAnchor.constraint(equalToConstant: 250),
-                scheduleLabel.heightAnchor.constraint(equalToConstant: 25)
-            ])
+                NSLayoutConstraint.activate([
+                    clockImageView.topAnchor.constraint(equalTo: surfBreakContainerView!.bottomAnchor, constant: 20),
+                    clockImageView.leadingAnchor.constraint(equalTo: detailsView.leadingAnchor, constant: 10), // Adjusted to leadingAnchor
+                    clockImageView.widthAnchor.constraint(equalToConstant: 15),
+                    clockImageView.heightAnchor.constraint(equalToConstant: 15),
+
+                    titleLabel.centerYAnchor.constraint(equalTo: clockImageView.centerYAnchor),
+                    titleLabel.leadingAnchor.constraint(equalTo: clockImageView.trailingAnchor, constant: 8),
+                    titleLabel.widthAnchor.constraint(equalToConstant: 220),
+                    titleLabel.heightAnchor.constraint(equalToConstant: 25),
+
+                    scheduleLabel.centerYAnchor.constraint(equalTo: clockImageView.centerYAnchor),
+                    scheduleLabel.trailingAnchor.constraint(equalTo: detailsView.trailingAnchor, constant: -10),
+                    scheduleLabel.widthAnchor.constraint(equalToConstant: 250),
+                    scheduleLabel.heightAnchor.constraint(equalToConstant: 25)
+                ])
             
             previousView = scheduleLabel
         }
@@ -176,21 +146,91 @@ class DetailSpotViewController: UIViewController {
             }
             
             // Add constraints for icon and detail view
-            NSLayoutConstraint.activate([
-                iconImageView.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: 10),
-                iconImageView.leftAnchor.constraint(equalTo: detailsView.leftAnchor, constant: 4),
-                iconImageView.widthAnchor.constraint(equalToConstant: 12),
-                iconImageView.heightAnchor.constraint(equalToConstant: 12),
-                
-                detailView.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: 10),
-                detailView.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 8),
-                detailView.rightAnchor.constraint(equalTo: detailsView.rightAnchor),
-                detailView.heightAnchor.constraint(equalToConstant: 25)
-            ])
-            
+                NSLayoutConstraint.activate([
+                    iconImageView.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: 11),
+                    iconImageView.leadingAnchor.constraint(equalTo: detailsView.leadingAnchor, constant: 10),
+                    iconImageView.widthAnchor.constraint(equalToConstant: 15),
+                    iconImageView.heightAnchor.constraint(equalToConstant: 15),
+
+                    detailView.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: 7),
+                    detailView.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 8),
+                    detailView.trailingAnchor.constraint(equalTo: detailsView.trailingAnchor, constant: -10),
+                    detailView.heightAnchor.constraint(equalToConstant: 25)
+                ])
             previousView = detailView
         }
     }
+    
+    // MARK: - Helper functions
+    
+    // Helper function to create a text with a background
+    private func createLabelWithBackground(text: String, backgroundColor: UIColor, cornerRadius: CGFloat) -> UILabel {
+        
+        let label = UILabel()
+        label.text = text
+        label.backgroundColor = backgroundColor
+        label.layer.cornerRadius = cornerRadius
+        label.clipsToBounds = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+    
+        
+        return label
+    }
+    
+    // Helper function to determine background color based on surf break
+    private func surfBreakBackgroundColor(surfBreak: String?) -> UIColor {
+        switch surfBreak {
+        case "Reef Break":
+            return .systemCyan
+        case "Point Break":
+            return .systemMint
+        case "Beach Break":
+            return .systemTeal
+        case "Outer Banks":
+            return .systemPink
+        default:
+            return .systemYellow
+        }
+    }
+
+    // Helper function to create an image view with a system icon
+    private func createIconImageView(systemName: String) -> UIImageView {
+        let imageView = UIImageView(image: UIImage(systemName: systemName))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }
+    
+    // Helper function to create a label with specified text and alignment
+    private func createLabel(text: String, alignment: NSTextAlignment = .left) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.textAlignment = alignment
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+    
+    // Helper function to create a row of stars based on difficulty level
+    private func createStarsRow(difficultyLevel: Int) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 4
+
+        // You can customize the star image based on your needs
+        let starImage = UIImage(systemName: "star.fill")
+        let starImageViews: [UIImageView] = (0..<difficultyLevel).map { _ in
+            let imageView = UIImageView(image: starImage)
+            imageView.tintColor = .systemOrange
+            return imageView
+        }
+
+        for starImageView in starImageViews {
+            stackView.addArrangedSubview(starImageView)
+        }
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }
+    
     
     // Create a UITextView for displaying a link with tap functionality
     private func createLinkTextView(url: URL, detail: String) -> UITextView {
