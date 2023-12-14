27d590de8b991c277305602b4146559e2cc78694
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-12-14 10:53:56 +0100

    make the link tappable using UITextView

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSunEarth/Controllers/DetailSpotViewController.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSunEarth/Controllers/DetailSpotViewController.swift
@@ -10,140 +10,146 @@ class DetailSpotViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Check if a spot is selected and set up the UI
         if let spot = selectedSpot {
-            self.setUpUI(spot: spot)
+            setUpUI(spot: spot)
         }
     }
     
+    // Set up the basic UI elements based on the selected spot
     func setUpUI(spot: Spot) {
-        self.spotImage.image = self.selectedImage
-        self.spotImage.layer.cornerRadius = self.spotImage.frame.size.width / 2
-        self.spotName.text = spot.fields.destination
-        self.setUpSpotDetails(spot: spot)
+        spotImage.image = selectedImage
+        spotImage.layer.cornerRadius = spotImage.frame.size.width / 2
+        spotName.text = spot.fields.destination
+        
+        // Set up the detailed information view for the spot
+        setUpSpotDetails(spot: spot)
     }
     
+    // Helper function to create an image view with a system icon
+    private func createIconImageView(systemName: String) -> UIImageView {
+        let imageView = UIImageView(image: UIImage(systemName: systemName))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }
+    
+    // Helper function to create a label with specified text and alignment
+    private func createLabel(text: String, alignment: NSTextAlignment = .left) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.textAlignment = alignment
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+    
+    // Set up the detailed information view for the spot, including peak surf season and contact details
     private func setUpSpotDetails(spot: Spot) {
+        // Peak surf season details
         let peakSurfSeason: [(title: String, dates: String)] = [
-            ("Peak Surf Season", "\(spot.fields.peakSurfSeasonBegins)・\( spot.fields.peakSurfSeasonEnds)")
+            ("Peak Surf Season", "\(spot.fields.peakSurfSeasonBegins)・\(spot.fields.peakSurfSeasonEnds)")
         ]
-        var previousView : UIView = self.detailsView
+        var previousView: UIView = detailsView
         
         for (title, dates) in peakSurfSeason {
-            let lineView = LineView()
-            lineView.setUpLineContent(title: title, schedule: dates)
-            self.detailsView.addSubview(lineView)
-            lineView.translatesAutoresizingMaskIntoConstraints = false
-            if previousView === self.detailsView {
-                lineView.topAnchor.constraint(equalTo: previousView.topAnchor).isActive = true
-            } else {
-                lineView.topAnchor.constraint(equalTo: previousView.bottomAnchor).isActive = true
-            }
+            // Create clock icon image view
+            let clockImageView = createIconImageView(systemName: "clock")
+            detailsView.addSubview(clockImageView)
             
-            lineView.leftAnchor.constraint(equalTo: self.detailsView.leftAnchor).isActive = true
-            lineView.rightAnchor.constraint(equalTo: self.detailsView.rightAnchor).isActive = true
-            lineView.heightAnchor.constraint(equalToConstant: 25).isActive = true
-            previousView = lineView
+            // Create title label
+            let titleLabel = createLabel(text: title)
+            detailsView.addSubview(titleLabel)
+            
+            // Create schedule label
+            let scheduleLabel = createLabel(text: dates, alignment: .right)
+            detailsView.addSubview(scheduleLabel)
+            
+            // Add constraints for clock icon, title, and schedule labels
+            NSLayoutConstraint.activate([
+                clockImageView.topAnchor.constraint(equalTo: previousView.topAnchor, constant: 4),
+                clockImageView.leftAnchor.constraint(equalTo: detailsView.leftAnchor, constant: 4),
+                clockImageView.widthAnchor.constraint(equalToConstant: 12),
+                clockImageView.heightAnchor.constraint(equalToConstant: 12),
+                
+                titleLabel.centerYAnchor.constraint(equalTo: clockImageView.centerYAnchor),
+                titleLabel.leftAnchor.constraint(equalTo: clockImageView.rightAnchor, constant: 8),
+                titleLabel.widthAnchor.constraint(equalToConstant: 220),
+                titleLabel.heightAnchor.constraint(equalToConstant: 25),
+                
+                scheduleLabel.centerYAnchor.constraint(equalTo: clockImageView.centerYAnchor),
+                scheduleLabel.rightAnchor.constraint(equalTo: detailsView.rightAnchor),
+                scheduleLabel.widthAnchor.constraint(equalToConstant: 250),
+                scheduleLabel.heightAnchor.constraint(equalToConstant: 25)
+            ])
+            
+            previousView = scheduleLabel
         }
         
+        // Contact details, including a link
         let contactDetails: [(icon: String, detail: String)] = [
             ("mappin.and.ellipse", spot.fields.destinationStateCountry),
             ("link", spot.fields.magicSeaweedLink)
         ]
         
-        for (icon,detail) in contactDetails {
-                let lineView2 = LineView2()
-                lineView2.setUpLineContent(icon: icon, detail: detail)
-                self.detailsView.addSubview(lineView2)
-                lineView2.translatesAutoresizingMaskIntoConstraints = false
-                lineView2.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: 10).isActive = true
-                lineView2.leftAnchor.constraint(equalTo: self.detailsView.leftAnchor).isActive = true
-                lineView2.rightAnchor.constraint(equalTo: self.detailsView.rightAnchor).isActive = true
-                lineView2.heightAnchor.constraint(equalToConstant: 25).isActive = true
-                previousView = lineView2
+        for (icon, detail) in contactDetails {
+            // Create icon image view
+            let iconImageView = createIconImageView(systemName: icon)
+            detailsView.addSubview(iconImageView)
+            
+            var detailView: UIView
+            
+            // Check if the detail is a link, and create a UITextView if so
+            if icon == "link", let url = URL(string: detail) {
+                let linkTextView = createLinkTextView(url: url, detail: detail)
+                detailsView.addSubview(linkTextView)
+                detailView = linkTextView
+            } else {
+                // Create a regular label for non-link details
+                let detailLabel = createLabel(text: detail)
+                detailsView.addSubview(detailLabel)
+                detailView = detailLabel
             }
-        
-        
-        
-    }
-}
-class LineView: UIView {
-    var title: String = ""
-    var schedule: String = ""
-    
-    func setUpLineContent(title: String,  schedule: String) {
-        self.title = title
-        self.schedule = schedule
-        
-        self.setUp()
-    }
-    
-    func setUp() {
-        let clockImageView: UIImageView = UIImageView(image: UIImage(systemName: "clock"))
-        self.addSubview(clockImageView)
-        
-        clockImageView.translatesAutoresizingMaskIntoConstraints = false
-        clockImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 4).isActive = true
-        clockImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 4).isActive = true
-        clockImageView.widthAnchor.constraint(equalToConstant: 12).isActive = true
-        clockImageView.heightAnchor.constraint(equalToConstant: 12).isActive = true
-        
-        let titleLabel: UILabel = UILabel()
-        titleLabel.text = self.title
-        self.addSubview(titleLabel)
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        titleLabel.leftAnchor.constraint(equalTo: clockImageView.rightAnchor, constant: 8).isActive = true
-        titleLabel.widthAnchor.constraint(equalToConstant: 220).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
-        
-        let scheduleLabel: UILabel = UILabel()
-        scheduleLabel.text = self.schedule
-        
-        scheduleLabel
-            .textAlignment = .right
-        self.addSubview(scheduleLabel)
-        
-        scheduleLabel.translatesAutoresizingMaskIntoConstraints = false
-        scheduleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        scheduleLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        scheduleLabel.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        scheduleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
-    }
-}
-
-class LineView2: UIView {
-    var icon: String = ""
-    var detail: String = ""
-    
-    func setUpLineContent(icon: String,  detail: String) {
-        self.icon = icon
-        self.detail = detail
-        
-        self.setUp()
+            
+            // Add constraints for icon and detail view
+            NSLayoutConstraint.activate([
+                iconImageView.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: 10),
+                iconImageView.leftAnchor.constraint(equalTo: detailsView.leftAnchor, constant: 4),
+                iconImageView.widthAnchor.constraint(equalToConstant: 12),
+                iconImageView.heightAnchor.constraint(equalToConstant: 12),
+                
+                detailView.topAnchor.constraint(equalTo: previousView.bottomAnchor, constant: 10),
+                detailView.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 8),
+                detailView.rightAnchor.constraint(equalTo: detailsView.rightAnchor),
+                detailView.heightAnchor.constraint(equalToConstant: 25)
+            ])
+            
+            previousView = detailView
+        }
     }
     
-    func setUp() {
-        let iconImageView: UIImageView = UIImageView(image: UIImage(systemName: icon))
-        self.addSubview(iconImageView)
+    // Create a UITextView for displaying a link with tap functionality
+    private func createLinkTextView(url: URL, detail: String) -> UITextView {
+        let textView = UITextView()
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.dataDetectorTypes = .link
+        textView.textColor = .systemBlue
+        textView.backgroundColor = .clear
+        textView.textContainer.lineFragmentPadding = 0
+        textView.textContainerInset = .zero
+        textView.linkTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.systemBlue,
+            NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.heightAnchor.constraint(equalToConstant: 25).isActive = true
         
-        iconImageView.translatesAutoresizingMaskIntoConstraints = false
-        iconImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 4).isActive = true
-        iconImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 4).isActive = true
-        iconImageView.widthAnchor.constraint(equalToConstant: 12).isActive = true
-        iconImageView.heightAnchor.constraint(equalToConstant: 12).isActive = true
+        // Create an attributed string with a link attribute
+        let attributedString = NSMutableAttributedString(string: "\(detail)")
+        attributedString.addAttribute(.link, value: url, range: NSRange(location: 0, length: detail.count))
         
-        let detailLabel: UILabel = UILabel()
-        detailLabel.text = self.detail
-        self.addSubview(detailLabel)
+        textView.attributedText = attributedString
         
-        detailLabel.translatesAutoresizingMaskIntoConstraints = false
-        detailLabel.translatesAutoresizingMaskIntoConstraints = false
-        detailLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        detailLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 8).isActive = true
-        detailLabel.widthAnchor.constraint(equalToConstant: 250).isActive = true
-        detailLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
-        
+        return textView
     }
 }
